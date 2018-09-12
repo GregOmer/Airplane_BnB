133dfd3a918bbe7df5d82f982ca4fc5ba6e867f0
Greg Omer <gregoryeomer@live.com>
2018-09-12 17:55:29 -0600

    html initiate

--- migrations/hangars.js
+++ migrations/hangars.js
@@ -2,7 +2,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('hangars', function(table) {
     table.increments()
     table.string('ap_name', 50).notNullable().defaultTo('')
-    table.number('number_hangars', 20).notNullable().defaultTo('')
+    table.number('number_hangars', 20).notNullable().defaultTo(0)
+    table.string('max_wingspan', 15).notNullable().defaultTo('')
+    table.string('max_length', 15).notNullable().defaultTo('')
+    table.string('max_height', 15).notNullable().defaultTo('')
     table.timestamps(true,true)
   })
 };

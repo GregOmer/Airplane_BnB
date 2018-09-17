731d470f9054abe8c9c2404b93d05a75c8bbbb73
Greg Omer <gregoryeomer@live.com>
2018-09-17 10:55:36 -0600

    beginning JS work

---
+++ public/home.js
@@ -0,0 +1,14 @@
+if(inputField1.data === ap_zip){
+  ap_result = document.createElement('div');
+  ap_result.classname = 'airport_result'
+  body.appendChild(ap_result);
+}
+
+
+
+// EXAMPLE APPEND
+// for(let i = 0; i < 900; i++){
+//   loopDiv = document.createElement("section");
+//   loopDiv.className = "pixel";
+//   wrapper.appendChild(loopDiv);
+// };
